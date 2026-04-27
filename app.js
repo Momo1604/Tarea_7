c6381c89280a9f22d305b0c59786103680aeabd2
Romulo Muñoz <romulo.munoz@iteso.mx>
2026-04-27 13:41:37 -0600

    app.js agregado

---
+++ app.js
@@ -0,0 +1,8 @@
+const moment = require('moment');
+let i = 5;
+let j  = 10;
+console.log('Iniciando...');
+console.log("La suma es " + (i + j));
+
+let fecha = moment().format('LL');
+console.log("La fecha de hoy es ", fecha);
